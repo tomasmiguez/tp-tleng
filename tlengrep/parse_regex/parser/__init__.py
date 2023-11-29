acf1b0e1f554cd87a3aa08862cc36271a62a5b35
mybelgo <mybelgo@gmail.com>
2023-11-29 17:58:28 -0300

    feat(parser): translate grammar to ply parser

---
+++ tlengrep/parse_regex/parser/__init__.py
@@ -0,0 +1,79 @@
+import re
+from lexer import tokens
+from tlengrep.regex import Char, Concat, Empty, Lambda, Plus, Star, Union
+
+def p_union(p):
+    '''
+    union : concat OR union
+    '''
+    p[0] = Union(p[1], p[3])
+
+def p_union_concat(p):
+    '''
+    union : concat
+    '''
+    p[0] = p[1]
+
+def p_concat(p):
+    '''
+    concat : op concat
+    '''
+    p[0] = Concat(p[1], p[2])
+
+def p_concat_lambda(p):
+    '''
+    concat :
+    '''
+    p[0] = Lambda()
+
+def p_op(p):
+    '''
+    op : val KLEEN
+       | val POSITIVE
+       | val OPTIONAL
+       | val
+    '''
+    if len(p) == 3:
+        if p[2] == '*':
+            p[0] = Star(p[1])
+        elif p[2] == '+':
+            p[0] = Plus(p[1])
+        elif p[2] == '?':
+            p[0] = Union(p[1], Lambda())
+    else:
+        p[0] = p[1]
+
+def p_op_range(p):
+    '''
+    p_op_range : val RANGE1
+               | val RANGE2
+    '''
+    unions = Empty()
+    for n in range(p[2].min, p[2].max):
+        concats = Lambda()
+        for _ in range(n):
+            concats = Concat(concats, p[1])
+        unions = Union(unions, concats)
+    p[0] = unions
+
+def p_val_paren(p):
+    '''
+    val : L_P union R_L
+    '''
+    p[0] = p[2]
+
+def p_val_char(p):
+    '''
+    val : CHAR
+        | ESCAPE
+    '''
+    p[0] = Char(p[1])
+
+def p_val_class(p):
+    '''
+    val : CLASS
+    '''
+    unions = Empty()
+    for sym in p[1].all_symbols():
+        unions = Union(unions, Char(sym))
+    p[0] = unions

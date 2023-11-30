d3712678b8b69beb005af2b2cd6abbe1e30ee6f1
mybelgo <mybelgo@gmail.com>
2023-11-30 15:05:33 -0300

    feat(lexer): add all_symbols to interval

--- tlengrep/parse_regex/lexer.py
+++ tlengrep/parse_regex/lexer.py
@@ -2,6 +2,7 @@ import re
 import ply.lex as lex
 from ply.lex import TOKEN
 
+
 class RegexRange:
     def __init__(self, min: int, max: int) -> None:
         self.min = min
@@ -13,16 +14,19 @@ class RegexRange:
 
         return self.min == other.min and self.max == other.max
 
+
 class RegexClassInterval:
     def __init__(self, fst: str, lst: str) -> None:
         self.fst = fst
         self.lst = lst
+        self.all_symbols = set([chr(i) for i in range(ord(fst), ord(lst) + 1)])
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, RegexClassInterval):
             return False
 
         return self.fst == other.fst and self.lst == other.lst
+
 
 tokens = (
     'CHAR',
@@ -34,10 +38,13 @@ tokens = (
 literals = "|*+?()[]"
 
 escaped = r'\\(.)'
+
+
 @TOKEN(escaped)
 def t_ESCAPED(t):
     t.value = t.lexer.lexmatch.group(2)
     return t
+
 
 def t_RANGE(t):
     r'\{(?:(?P<min>\d+),)?(?P<max>\d+)\}'
@@ -49,13 +56,19 @@ def t_RANGE(t):
 
     return t
 
+
 t_CHAR = r'[^' + re.escape(literals) + r'\\' + r']'
 
 char_or_escape = r'(?:' + t_CHAR + r'|' + escaped + r')'
 class_int = r'(?P<fst>' + char_or_escape + r')-(?P<snd>' + char_or_escape + r')'
+
+
 @TOKEN(class_int)
 def t_CLASS_INT(t):
-    t.value = RegexClassInterval(t.lexer.lexmatch.group('fst'), t.lexer.lexmatch.group('snd'))
+    t.value = RegexClassInterval(
+        t.lexer.lexmatch.group('fst'), t.lexer.lexmatch.group('snd')
+    )
     return t
+
 
 lexer = lex.lex()
